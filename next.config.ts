f9986027685807aa2b716ff9fd9d74f5bef89037
Eliot Kid <eliotfonse99@gmail.com>
2026-03-05 12:39:53 -0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "http://localhost:3000/",
+        permanent: false,
+      },
+      {
+        source: "/tengu",
+        destination: "http://localhost:3000/tengu",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
